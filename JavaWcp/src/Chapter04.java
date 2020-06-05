b7809812790c32af8ea33d9372dae61d03068d77
Haruka W <nekonoko1995@gmail.com>
2020-06-05 16:55:27 +0900

    [Finish]基礎ー4章

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		String str1;
+		str1 = "渡邊晴香";
+		System.out.println(str1);
+
+		str1 = "Cat";
+		System.out.println(str1);
+
+		final int lucky_number = 713;
+		System.out.println(lucky_number);
+	}
+}
