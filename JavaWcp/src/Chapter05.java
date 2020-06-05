2df65eb40291414dcb75fec38bdc0a3e4b62f3c5
Haruka W <nekonoko1995@gmail.com>
2020-06-05 18:29:01 +0900

    [Finish]基礎ー5章

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,28 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		//プリミティブ型
+		long long1 = 1234567890L;
+		System.out.println(long1);
+
+		double double1 = 12345.67d;
+		System.out.println(double1);
+
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+
+		//ラッパークラス
+		Float float1 = 0.1234f;
+		System.out.println(float1);
+
+		Integer integer1 = 123;
+		System.out.println(integer1);
+
+		Character character1 = 12354;
+		System.out.println(character1);
+
+		//配列
+		int[] array1 = { 0, 1, 2, 3, 4 };
+		System.out.println(array1[3]);
+	}
+}
